555cbf3b76d392c245f1fcffe0db32d8a3863cd9
Trevor Bernard <trevor.bernard@radian6.com>
2011-04-01 15:53:16 -0300

    Added special hooks if you are running emacs on macs

--- theme.el
+++ theme.el
@@ -1,19 +1,25 @@
 (require 'color-theme)
 
 (defun os-x-theme ()
-  (setq default-frame-alist
-        '((left . 22) (top . 44)
-          (width . 100) (height . 50)))
-  (setq mac-allow-anti-aliasing t)
-  (set-face-font
-   'default
-   "-apple-DejaVu_Sans_Mono-medium-normal-normal-*-12-*-*-*-m-0-iso10646-"))
+  ;; (setq default-frame-alist
+  ;;       '((left . 22) (top . 44)
+  ;;         (width . 100) (height . 50)))
+)
 
 
 (color-theme-initialize)
 ;; (load-library "color-theme-underwater.el")
 (load-library "color-theme-sunburst.el")
 (color-theme-tm)
+(if (eq system-type 'darwin)
+    (progn
+      (setq mac-allow-anti-aliasing t)
+      (set-face-font
+       'default
+       "-apple-DejaVu_Sans_Mono-medium-normal-normal-*-12-*-*-*-m-0-iso10646-")))
+
+(if (eq system-type 'darwin)
+    (setenv "PATH" "/usr/local/bin:/opt/local/bin:/opt/local/sbin:/usr/bin:/bin:/usr/sbin:/sbin"))
 (os-x-theme)
 
 (provide 'theme)
