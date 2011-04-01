b23d7f4127911cef3bfe7068cdc11bfdb061574e
Trevor Bernard <trevor.bernard@radian6.com>
2011-04-01 13:52:09 -0300

    Added javascript to the mix

--- my-javascript.el
+++ my-javascript.el
@@ -1,3 +1,3 @@
-;(load "~/.emacs.d/vendor/js2-20090723b.el")
-;(autoload 'js2-mode "js2" nil t)
-;(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+(load "~/.emacs.d/vendor/js2-20090723b.elc")
+(autoload 'js2-mode "js2" nil t)
+(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
