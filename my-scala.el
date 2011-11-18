56d117602dde48297fde6015858befce4fb308f7
Trevor Bernard <trevor.bernard@gmail.com>
2011-11-18 09:59:04 -0400

    Fixed a crash since we haven't completely fixed ensime yet.

--- my-scala.el
+++ my-scala.el
@@ -3,7 +3,7 @@
 (setq scala-mode-indent:step 2)
 (require 'scala-mode)
 (add-to-list 'auto-mode-alist '("\\.scala$" . scala-mode))
-(add-to-list 'load-path "~/.emacs.d/vendor/ensime/dist/elisp/")
+;(add-to-list 'load-path "~/.emacs.d/vendor/ensime/dist/elisp/")
 
-(require 'ensime)
-(add-hook 'scala-mode-hook 'ensime-scala-mode-hook)
+;(require 'ensime)
+;(add-hook 'scala-mode-hook 'ensime-scala-mode-hook)
