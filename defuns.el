d33a4e79a4587bf753e25b04f8d82179af46fda8
Trevor Bernard <trevor.bernard@gmail.com>
2011-11-24 21:32:03 -0400

    Added emacs prelude

--- defuns.el
+++ defuns.el
@@ -1,3 +1,13 @@
+(defun prelude-google ()
+  "Googles a query or region if any."
+  (interactive)
+  (browse-url
+   (concat
+    "http://www.google.com/search?ie=utf-8&oe=utf-8&q="
+    (if mark-active
+        (buffer-substring (region-beginning) (region-end))
+      (read-string "Google: ")))))
+
 (defun maximize-frame ()
   (interactive)
   (set-frame-size (selected-frame)
