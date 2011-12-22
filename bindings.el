80df895572a0dd7a0d11f938fca3947cfe20bdaa
Trevor Bernard <trevor.bernard@gmail.com>
2011-12-22 11:31:25 -0400

    Added various modes but most importantly clojure.. not competely cross platform

--- bindings.el
+++ bindings.el
@@ -7,5 +7,4 @@
 ;; Allow M-x to be accessed by ctrl
 (global-set-key "\C-x\C-m" 'execute-extended-command)
 (global-set-key "\C-c\C-m" 'execute-extended-command)
-;;(global-set-key "\C-l" 'goto-line)
 (provide 'bindings)
--- init.el
+++ init.el
@@ -24,9 +24,36 @@
 (load-library "my-nxml.el")
 (load-library "my-javascript.el")
 (load-library "my-php.el")
-(load-library "my-clojure.el")
+;;(load-library "my-clojure.el")
 (load-library "my-protobuf.el")
 
 ;; Sending mail
 (setq user-full-name "Trevor Bernard")
 (setq user-full-mail-address "trevor.bernard@gmail.com")
+
+;; (add-to-list 'load-path "~/.emacs.d/packages/")
+;; (require 'package)
+;; (add-to-list 'package-archives
+;; '("marmalade" . "http://marmalade-repo.org/packages/"))
+;; (package-initialize)
+
+;; Temporary clojure/slime setup
+;; clojure-mode
+(add-to-list 'load-path "~/clojure-mode")
+(require 'clojure-mode)
+
+;; paredit
+;;(add-to-list 'load-path "~/opt/paredit")
+(require 'paredit)
+
+;; slime
+(eval-after-load "slime" 
+  '(progn (slime-setup '(slime-repl))	
+	(defun paredit-mode-enable () (paredit-mode 1))	
+	(add-hook 'slime-mode-hook 'paredit-mode-enable)	
+	(add-hook 'slime-repl-mode-hook 'paredit-mode-enable)
+	(setq slime-protocol-version 'ignore)))
+
+(add-to-list 'load-path "~/slime")
+(require 'slime)
+(slime-setup)
