de63522b6188ebb939e974fe66e8bb72219a24a8
Trevor Bernard <trevor.bernard@gmail.com>
2012-04-14 15:44:02 -0400

    Slowing cleaning up the elisp

--- platform.el
+++ platform.el
@@ -1,7 +1,9 @@
-(if (eq system-type 'darwin)
-    (progn
-      (menu-bar-mode 1)
-      (setenv "PATH" "/usr/local/bin:/opt/local/bin:/opt/local/sbin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/scala/bin")
-      (setq exec-path (append exec-path (list "/usr/local/bin" "/usr/local/scala/bin" "~/bin")))))
+(when (equal system-type 'darwin)
+  ;; I'm going to eventually clean up this list
+  (setenv "PATH" "/usr/local/bin:/opt/local/bin:/opt/local/sbin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/scala/bin")
+  (setq exec-path (append exec-path (list "/usr/local/bin" "/usr/local/scala/bin" "~/bin")))
+  ;; (setenv "PATH" (concat "/opt/local/bin:/usr/local/bin:" (getenv "PATH")))
+  ;; (push "/opt/local/bin" exec-path)
+  )
 
 (provide 'platform)
