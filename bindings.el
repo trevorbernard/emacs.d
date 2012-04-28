fbdc361eea0a25c8faeeb90d7de19051941d129e
Trevor Bernard <trevor.bernard@gmail.com>
2012-04-28 09:55:36 -0300

    Bind C-c C-j to clojure jack in

--- bindings.el
+++ bindings.el
@@ -6,6 +6,6 @@
 ;; Allow M-x to be accessed by ctrl
 (global-set-key "\C-x\C-m" 'execute-extended-command)
 (global-set-key "\C-c\C-m" 'execute-extended-command)
-
+(global-set-key (kbd "C-c C-j") 'clojure-jack-in)
 (global-set-key '[f3] 'slime-eval-print-last-expression)
 (provide 'bindings)
