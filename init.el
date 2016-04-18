aee5ca5bdb3d4220ac2c473b904c4404dd155573
Trevor Bernard <trevor.bernard@gmail.com>
2016-04-18 16:48:54 -0300

    Fix error after upgrade to cider 0.12.0

--- init.el
+++ init.el
@@ -67,7 +67,7 @@
 (add-hook 'cider-repl-mode-hook 'paredit-mode)
 (add-hook 'cider-repl-mode-hook 'rainbow-delimiters-mode)
 (add-hook 'cider-mode-hook 'company-mode)
-(add-hook 'cider-mode-hook 'cider-turn-on-eldoc-mode)
+(add-hook 'cider-mode-hook 'eldoc-mode)
 
 (add-hook 'clojure-mode-hook
           (lambda ()
