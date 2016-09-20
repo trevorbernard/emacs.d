0eada285483869a9b738ea25f9371fb0005550ce
Trevor Bernard <trevor.bernard@gmail.com>
2016-09-20 17:53:57 -0300

    Saving emacs.d

--- init.el
+++ init.el
@@ -154,7 +154,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (yaml-mode mmm-mode protobuf-mode markdown-mode rainbow-delimiters company cider projectile zenburn-theme clojure-mode paredit))))
+    (less-css-mode terraform-mode yaml-mode mmm-mode protobuf-mode markdown-mode rainbow-delimiters company cider projectile zenburn-theme clojure-mode paredit))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
