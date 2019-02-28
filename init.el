3c62a6979b703a10a419c7f902502bf914fc401d
Trevor Bernard <trevor.bernard@gmail.com>
2019-02-28 13:22:19 -0500

    Eclim stuff

--- init.el
+++ init.el
@@ -8,7 +8,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (elpy htmlize dockerfile-mode clj-refactor yaml-mode rainbow-delimiters protobuf-mode projectile paredit markdown-mode dracula-theme company cider))))
+    (eclim ag gradle-mode elpy htmlize dockerfile-mode clj-refactor yaml-mode rainbow-delimiters protobuf-mode projectile paredit markdown-mode dracula-theme company cider))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
