4023ba6036d5c089ae238540bae16a224395c01a
Trevor Bernard <trevor.bernard@gmail.com>
2023-02-05 16:01:33 -0500

    Making the emacs -nw pretty

--- init.el
+++ init.el
@@ -7,7 +7,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(terraform-mode sbt-mode elixir-mode bnf-mode eclim ag gradle-mode elpy htmlize dockerfile-mode clj-refactor yaml-mode rainbow-delimiters protobuf-mode projectile paredit markdown-mode dracula-theme company cider))
+   '(nix-mode terraform-mode sbt-mode elixir-mode bnf-mode eclim ag gradle-mode elpy htmlize dockerfile-mode clj-refactor yaml-mode rainbow-delimiters protobuf-mode projectile paredit markdown-mode dracula-theme company cider))
  '(safe-local-variable-values '((cider-shadow-cljs-default-options . "app"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
