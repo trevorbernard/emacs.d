1aa7c8205950dc22155f6dd43344f5b7fe1ca5c3
Trevor Bernard <trevor.bernard@gmail.com>
2023-10-27 12:23:23 -0400

    Add support for converting org to github flavoured markdown

--- init.el
+++ init.el
@@ -6,8 +6,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(org-export-backends '(ascii html icalendar latex md odt))
  '(package-selected-packages
-   '(yasnippet-snippets graphql-mode go-mode lsp-mode rustic edit-indirect tuareg ## nix-mode terraform-mode sbt-mode elixir-mode bnf-mode eclim ag gradle-mode elpy htmlize dockerfile-mode clj-refactor yaml-mode rainbow-delimiters protobuf-mode projectile paredit markdown-mode dracula-theme company cider))
+   '(ox-gfm just-mode yasnippet-snippets graphql-mode go-mode lsp-mode rustic edit-indirect tuareg ## nix-mode terraform-mode sbt-mode elixir-mode bnf-mode eclim ag gradle-mode elpy htmlize dockerfile-mode clj-refactor yaml-mode rainbow-delimiters protobuf-mode projectile paredit markdown-mode dracula-theme company cider))
  '(safe-local-variable-values '((cider-shadow-cljs-default-options . "app"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
