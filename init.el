c33bfc2715fb558eeafaf2a63a7c9c2c6bb2abc9
Trevor Bernard <trevor.bernard@gmail.com>
2023-11-14 09:21:14 -0500

    Add some rust stuff

--- init.el
+++ init.el
@@ -8,7 +8,7 @@
  ;; If there is more than one, they won't work right.
  '(org-export-backends '(ascii html icalendar latex md odt))
  '(package-selected-packages
-   '(ox-gfm just-mode yasnippet-snippets graphql-mode go-mode lsp-mode rustic edit-indirect tuareg ## nix-mode terraform-mode sbt-mode elixir-mode bnf-mode eclim ag gradle-mode elpy htmlize dockerfile-mode clj-refactor yaml-mode rainbow-delimiters protobuf-mode projectile paredit markdown-mode dracula-theme company cider))
+   '(lsp-ui ox-gfm just-mode yasnippet-snippets graphql-mode go-mode lsp-mode rustic edit-indirect tuareg ## nix-mode terraform-mode sbt-mode elixir-mode bnf-mode eclim ag gradle-mode elpy htmlize dockerfile-mode clj-refactor yaml-mode rainbow-delimiters protobuf-mode projectile paredit markdown-mode dracula-theme company cider))
  '(safe-local-variable-values '((cider-shadow-cljs-default-options . "app"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
