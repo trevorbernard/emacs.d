2b52b9d6a4d88a497c169529b638add8900181a6
Trevor Bernard <trevor.bernard@gmail.com>
2024-02-05 20:02:06 -0500

    Add just-mode

--- init.el
+++ init.el
@@ -1,3 +1,16 @@
 (require 'org)
 
 (org-babel-load-file "~/.emacs.d/configuration.org")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(just-mode yaml-mode tuareg terraform-mode rustic rainbow-delimiters protobuf-mode projectile ox-gfm nix-mode magit lsp-ui lsp-mode htmlize gradle-mode flycheck-plantuml eclim dracula-theme dockerfile-mode company clj-refactor cider transient sesman bnf-mode ag)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
