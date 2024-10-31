092cb04c021e05524f0d2dc33d0cb4b514e87f9f
Trevor Bernard <trevor.bernard@gmail.com>
2024-10-31 14:41:21 -0400

    Add Nix flake support

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "An Emacs development environment";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachSystem
+    [
+      "aarch64-darwin"
+      "x86_64-linux"
+    ]
+    (
+      system: let
+        pkgs = import nixpkgs { inherit system; };
+      in
+        rec {
+          devShells.default = pkgs.mkShell {
+            buildInputs = [
+              pkgs.emacs29
+              pkgs.just
+            ];
+            shellHook = ''
+              echo "Welcome to the Emacs development environment"
+            '';
+          };
+
+        }
+    );
+}
