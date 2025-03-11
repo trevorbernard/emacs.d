df2b0cca864fb02ec03e567c68706cbb6ae51fbd
Trevor Bernard <trevor.bernard@gmail.com>
2025-03-10 21:55:56 -0400

    emacs: Use native compilation for moar perf

--- init.el
+++ init.el
@@ -1,3 +1,13 @@
+(when (and (fboundp 'native-comp-available-p)
+           (native-comp-available-p))
+  (setq native-comp-async-report-warnings-errors nil)
+  (setq native-comp-speed 2)
+  (setq native-comp-jit-compilation t)
+  (when (boundp 'native-comp-eln-load-path)
+    (add-to-list 'native-comp-eln-load-path
+                 (expand-file-name "eln-cache/" user-emacs-directory)))
+  (setq native-comp-async-jobs-number 4))
+
 (let ((file-name-handler-alist nil))
   ;; If config is pre-compiled, then load that
   (if (file-exists-p (expand-file-name "configuration.elc" user-emacs-directory))
@@ -24,12 +34,12 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(default ((t (:inherit nil :extend nil :stipple nil :background "#2b303b" :foreground "#c0c5ce" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight regular :height 200 :width normal :foundry "nil" :family "Fira Code"))))
- '(org-document-title ((t (:inherit default :weight bold :foreground "#c0c5ce" :font "ETBembo" :height 2.0 :underline nil))))
- '(org-level-1 ((t (:inherit default :weight bold :foreground "#c0c5ce" :font "ETBembo" :height 1.75))))
- '(org-level-2 ((t (:inherit default :weight bold :foreground "#c0c5ce" :font "ETBembo" :height 1.5))))
- '(org-level-3 ((t (:inherit default :weight bold :foreground "#c0c5ce" :font "ETBembo" :height 1.25))))
- '(org-level-4 ((t (:inherit default :weight bold :foreground "#c0c5ce" :font "ETBembo" :height 1.1))))
- '(org-level-5 ((t (:inherit default :weight bold :foreground "#c0c5ce" :font "ETBembo"))))
- '(org-level-6 ((t (:inherit default :weight bold :foreground "#c0c5ce" :font "ETBembo"))))
- '(org-level-7 ((t (:inherit default :weight bold :foreground "#c0c5ce" :font "ETBembo"))))
- '(org-level-8 ((t (:inherit default :weight bold :foreground "#c0c5ce" :font "ETBembo")))))
+ '(org-document-title ((t (:inherit default :weight bold :foreground "Black" :font "ETBembo" :height 2.0 :underline nil))))
+ '(org-level-1 ((t (:inherit default :weight bold :foreground "Black" :font "ETBembo" :height 1.75))))
+ '(org-level-2 ((t (:inherit default :weight bold :foreground "Black" :font "ETBembo" :height 1.5))))
+ '(org-level-3 ((t (:inherit default :weight bold :foreground "Black" :font "ETBembo" :height 1.25))))
+ '(org-level-4 ((t (:inherit default :weight bold :foreground "Black" :font "ETBembo" :height 1.1))))
+ '(org-level-5 ((t (:inherit default :weight bold :foreground "Black" :font "ETBembo"))))
+ '(org-level-6 ((t (:inherit default :weight bold :foreground "Black" :font "ETBembo"))))
+ '(org-level-7 ((t (:inherit default :weight bold :foreground "Black" :font "ETBembo"))))
+ '(org-level-8 ((t (:inherit default :weight bold :foreground "Black" :font "ETBembo")))))
