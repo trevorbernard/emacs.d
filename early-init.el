727b5cbfa9692be9b66c8b82c53269c59e7a1a8f
Trevor Bernard <trevor.bernard@gmail.com>
2025-04-28 16:49:51 -0400

    chore: improving startup colour issues
    - import nix

--- early-init.el
+++ early-init.el
@@ -1,16 +1,24 @@
 ;;; -*- lexical-binding: t -*-
-(when window-system
-  (add-to-list 'default-frame-alist '(fullscreen . maximized))
-  (cond
-   ((eq system-type 'darwin)
-    (set-frame-font "Fira Code Retina 20" nil t))
-   ((eq system-type 'gnu/linux)
-    (set-frame-font "Fira Code 11" nil t))))
 
-(menu-bar-mode -1)
-(scroll-bar-mode -1)
-(tool-bar-mode -1)
-(tooltip-mode -1)
+(cond
+ ((eq system-type 'darwin)
+  (set-face-attribute 'default nil :font "Fira Code Retina-20"))
+ ((eq system-type 'gnu/linux)
+  (set-face-attribute 'default nil :font "Fira Code-11")))
+
+(push '(menu-bar-lines . 0) default-frame-alist)
+(push '(tool-bar-lines . 0) default-frame-alist)
+(push '(vertical-scroll-bars) default-frame-alist)
+(push '(fullscreen . maximized) initial-frame-alist)
+(push '(ns-transparent-titlebar . t) default-frame-alist)
+
+(set-face-attribute 'default nil :background "#282c34" :foreground "#bbc2cf")
 
 (setq inhibit-startup-message t
-      inhibit-startup-echo-area-message t)
+      inhibit-startup-echo-area-message t
+      frame-inhibit-implied-resize t
+      frame-resize-pixelwise t)
+
+(provide 'early-init)
+
+;;; early-init.el ends here
