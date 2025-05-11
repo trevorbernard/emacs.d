7e20af07e90fe4ab79ed8c5e511553babdfa58a2
Trevor Bernard <trevor.bernard@gmail.com>
2025-05-11 17:23:06 -0400

    chore: use pinned flakehub instead on unstable

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.2411.716632";
 
   outputs = { self, nixpkgs, ... }: let
     supportedSystems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
