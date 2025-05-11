48b66c98cbdae5fc88e02c6dab8a6238d66bf8f5
Trevor Bernard <trevor.bernard@gmail.com>
2025-05-11 00:12:12 -0400

    chore: move away from async compilation when using make
    TODO: doesn't quite work yet

--- lisp/compile.el
+++ lisp/compile.el
@@ -1,4 +1,4 @@
-  ;;; -*- lexical-binding: t -*-
+;;; compile.el --- tangle configuration.org -*- lexical-binding: t -*-
 (require 'org)
 
 (org-babel-tangle-file "configuration.org")
@@ -10,14 +10,13 @@
     (progn
       (message "Using native compilation")
       (setq native-comp-speed 2)
-      (setq native-comp-async-report-warnings-errors nil)
-      (when (fboundp 'native-compile-async)
-        (native-compile-async "configuration.el" t)
-        (native-compile-async "early-init.el" t)
-        (native-compile-async "init.el" t)))
+      (when (fboundp 'native-compile)
+        (native-compile "configuration.el")
+        (native-compile "init.el")))
   (message "Native compilation not available, using byte compilation")
   (byte-compile-file "configuration.el")
-  (byte-compile-file "early-init.el")
   (byte-compile-file "init.el"))
 
 (provide 'compile)
+
+;;; compile.el ends here
