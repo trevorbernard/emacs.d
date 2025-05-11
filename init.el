f1013e5f4812a87b29ff6075c6b1a161ad107de2
Trevor Bernard <trevor.bernard@gmail.com>
2025-05-11 00:12:50 -0400

    chore: remove custom org glyphs until I fix it

--- init.el
+++ init.el
@@ -30,17 +30,3 @@
            (add-to-list 'lsp-file-watch-ignored-directories "[/\\\\]\\.cargo\\'")
            (add-to-list 'lsp-file-watch-ignored-directories "[/\\\\]\\ops\\'")
            (add-to-list 'lsp-file-watch-ignored-directories "[/\\\\]\\docs\\'")))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(org-document-title ((t (:inherit default :weight bold :foreground "black" :family "Sans Serif" :height 2.0 :underline nil))))
- '(org-level-1 ((t (:inherit default :weight bold :foreground "black" :family "Sans Serif" :height 1.75))))
- '(org-level-2 ((t (:inherit default :weight bold :foreground "black" :family "Sans Serif" :height 1.5))))
- '(org-level-3 ((t (:inherit default :weight bold :foreground "black" :family "Sans Serif" :height 1.25))))
- '(org-level-4 ((t (:inherit default :weight bold :foreground "black" :family "Sans Serif" :height 1.1))))
- '(org-level-5 ((t (:inherit default :weight bold :foreground "black" :family "Sans Serif"))))
- '(org-level-6 ((t (:inherit default :weight bold :foreground "black" :family "Sans Serif"))))
- '(org-level-7 ((t (:inherit default :weight bold :foreground "black" :family "Sans Serif"))))
- '(org-level-8 ((t (:inherit default :weight bold :foreground "black" :family "Sans Serif")))))
