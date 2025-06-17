8aa222dbdede59e81176c91f41499301a8f5f983
Trevor Bernard <trevor.bernard@gmail.com>
2025-06-16 21:02:23 -0400

    chore: remove 'vertical-border in early-init.el and move to init.el

--- early-init.el
+++ early-init.el
@@ -1,7 +1,5 @@
 ;;; -*- lexical-binding: t -*-
 
-(set-display-table-slot standard-display-table 'vertical-border ?|)
-
 ;; Set frame parameters before frame creation
 (setq default-frame-alist
       (append default-frame-alist
